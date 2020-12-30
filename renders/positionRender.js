88131c2e315d74247c79217145a77a552562d4e3
Pavel Sukonkin <psukonkin@gmail.com>
2020-12-30 16:54:53 +0300

    up

--- renders/positionRender.js
+++ renders/positionRender.js
@@ -3,35 +3,42 @@ import { RenderWrap } from "./renderWrap.js";
 
 export class SortingRender extends RenderWrap {
 
-	constructor(render) {
+	constructor(render, sortBy) {
 		super();
 		this.#render = render;
 	}
 
 	#cache = [];
+	sort(){
+		this.#cache
+
+	// 	if (this.#sortBy.x)
+	// 	objects.sort((o1, o2) => { o1.x - o2.x })
+	// if (this.#sortBy.y)
+	// 	objects.sort((o1, o2) => { o1.y - o2.y })
+	// if (this.#sortBy.z)
+	this.#cache.sort((o1, o2) => { o1.z - o2.z })
+
+	}
 
 	clear(x, y, wi, he) {
-		this.render.clear(x + this.#x, y + this.#y, wi, he);
+		this.render.clear(x, y, wi, he);
 	}
 
 	rect(x, y, wi, he, color, fill) {
-		this.render.rect(x + this.#x, y + this.#y, wi, he, color, fill);
+		this.#cache.push({ func: 'rect', x: x, y: y, wi: wi, he: he, color: color, fill: fill })
 	}
 
 	circle(x, y, radius, color, fill) {
-		this.render.circle(x + this.#x, y + this.#y, radius, color, fill);
 	}
 
 	text(x, y, text, color, font, fill) {
-		this.render.text(x + this.#x, y + this.#y, text, color, font, fill);
 	}
 
 	sprite(x, y, wi, he, image) {
-		this.render.sprite(x + this.#x, y + this.#y, wi, he, image);
 	}
 
 	tile(x, y, wi, he, tiX, tiY, tiWi, tiHe, image) {
-		this.render.tile(x + this.#x, y + this.#y, wi, he, tiX, tiY, tiWi, tiHe, image);
 	}
 }
 
