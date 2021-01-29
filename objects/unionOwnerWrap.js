09fada5b4f712038adabe468bc490eee5939b87b
Pavel Sukonkin <psukonkin@gmail.com>
2021-01-29 15:35:49 +0300

    refac UnionOwnerWrap

--- objects/unionOwnerWrap.js
+++ objects/unionOwnerWrap.js
@@ -1,17 +1,27 @@
 import { OwnerWrap } from "./owner.js";
 
-const containers = []
+// const containers = []
+const owners = []
 export class UnionOwnerWrap extends OwnerWrap {
 
 	add(object) {
-		const container = containers.find(con => con.object == object)
-		if (container) {
-			container.owner.del(object.name)
-			container.owner = this
+		// const container = containers.find(con => con.object == object)
+		const owner = owners.find(ow => ow.objects.find(obj => obj == object))
+
+		// if (container) {
+		// 	container.owner.del(object.name)
+		// 	container.owner = this
+		// }
+		// else {
+		// 	containers.push({ object: object, owner: this })
+		// }
+
+		if (owner) {
+			if (owner == this) return
+			owner.del(object.name)
 		}
-		else {
-			containers.push({ object: object, owner: this })
-		}
+		else owners.push(this)
+
 		this.add(object)
 	}
 
